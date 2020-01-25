484d21ee5028249f200b6c1c1ec93db157fbe41d
Scott Hoisington <hoyzington@gmail.com>
2020-01-25 01:58:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+  
+  attr_accessor :name, :type
+  attr_reader :id, :db
+  
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save
+    p = self.new()
+      sql = "INSERT INTO pokemon (name, type, db) VALUES (?, ?, ?)"
+      DB[:conn].execute(sql, self.name, self.type, self.db)
+      @id = @db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+  end
+  
+  def update
+    sql = "UPDATE pokemon SET name = ?, type = ?, db = ? WHERE id = ?"
+    @db.execute(sql, self.name, self.type, self.db, self.id)
+  end
+  
 end
